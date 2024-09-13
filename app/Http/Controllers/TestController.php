40d1b9bc18d04868c038d88f99e2c86a54e159e7
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-09-13 14:20:53 +0600

    Selecting First Row, Seleting By ID, Selecting Specific Column, Row Count, Find-out Max Price, Find-out Min Price, Find-Out Average Price

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,7 +11,30 @@ class TestController extends Controller
     {
         
         // Selecting All Rows
-       $result = DB::table('brands')->get();
+    //    $result = DB::table('brands')->get();
+
+    
+    //Selecting First Rows
+    // $result = DB::table('brands')->first();
+
+    // Selecting  by ID
+    // $result = DB::table('brands')->find(4);
+
+    // Selecting Specific Column
+    // $result = DB::table('brands')->pluck('brandName');
+
+    
+    // Row Count
+    // $result = DB::table('products')->count();
+
+    // Find-Out Max Price
+    // $result = DB::table('products')->max('price');
+
+    // Find-Out Min Price
+    $result = DB::table('products')->min('price');
+
+    // Find-out average Price
+    $result = DB::table('products')->avg('price');
 
        return $result;
     }
