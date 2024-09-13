6b942aa923cae47e7a2168a0359ee87abee4dd5a
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-09-13 11:02:24 +0600

    Seleting All Rows

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    function test1(){
-        return 299;
+    function test1()
+    {
+        
+        // Selecting All Rows
+       $result = DB::table('brands')->get();
+
+       return $result;
     }
 }
